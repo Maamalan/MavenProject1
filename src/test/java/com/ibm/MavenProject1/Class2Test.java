adf12288b120bcd09c39b5f7e20f0df1022abe9c
Maria Amalan <maamalan@in.ibm.com>
2018-06-07 11:52:02 +0530

    Added new method

---
+++ src/test/java/com/ibm/MavenProject1/Class2Test.java
@@ -0,0 +1,14 @@
+package com.ibm.MavenProject1;
+
+import org.junit.Test;
+
+public class Class2Test {
+	
+	@Test
+	public void MyMethod1()
+	{
+		for (int i=1;i<5;i++)
+		System.out.println("My method.."+ i);
+	}
+
+}
