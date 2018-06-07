174d43c68b0a35e3dabbecef63b1f8ea14c5a398
Maria Amalan <maamalan@in.ibm.com>
2018-06-07 10:44:43 +0530

    Creating new project in git and committing the original files

---
+++ src/test/java/com/ibm/MavenProject1/Class1Test.java
@@ -0,0 +1,31 @@
+package com.ibm.MavenProject1;
+
+import org.junit.Test;
+
+public class Class1Test {
+	
+	@Test
+	public void FirstTest()
+	{
+		System.out.println("Hello FirstTest");
+	}
+
+	@Test
+	public void SecondTest()
+	{
+		System.out.println("Hello SecondTest");
+	}
+	
+	@Test
+	public void ThirdTest()
+	{
+		System.out.println("Hello ThirdTest");
+	}
+	
+	@Test
+	public void HelloWorld()
+	{
+		for (int i=1;i<=10;i++)
+		System.out.println("Hello World ... "+ i);
+	}
+}
