5cbb3ef82b6c5fa0b10de384f8db140c7371baf9
Maria Amalan <maamalan@in.ibm.com>
2018-06-25 15:07:56 +0530

    jenkins commit

--- src/test/java/com/ibm/MavenProject1/Class1Test.java
+++ src/test/java/com/ibm/MavenProject1/Class1Test.java
@@ -8,6 +8,6 @@ public class Class1Test {
 	public void HelloWorld()
 	{
 		for (int i=1;i<=10;i++)
-		System.out.println("Hello World ... "+ i);
+		System.out.println("Hello World ... hi back to you"+ i);
 	}
 }
